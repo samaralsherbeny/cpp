ffd5962043a8e71c2e0d84d2c7ae059a35d6463a
samaralsherbeny <samaralsherbeny2@gmail.com>
2026-07-23 17:37:22 +0300

    Add User class and derived classes for roles

---
+++ task14-Library using smart pointers/User.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <string>
+
+class User {
+protected:
+    std::string name;
+    int user_id;
+
+public:
+    User(std::string name, int user_id);
+    virtual void display_info() const = 0;
+    virtual ~User() = default;
+};
+
+class Student : public User {
+public:
+    Student(std::string name, int user_id);
+    void display_info() const override;
+};
+
+class Teacher : public User {
+public:
+    Teacher(std::string name, int user_id);
+    void display_info() const override;
+};
+
+class Engineer : public User {
+public:
+    Engineer(std::string name, int user_id);
+    void display_info() const override;
+};
