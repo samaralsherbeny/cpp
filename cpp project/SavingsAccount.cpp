a255de03c26494200c3d32545a38f64ea224d3c5
samaralsherbeny <samaralsherbeny2@gmail.com>
2026-07-28 20:42:08 +0300

    Add SavingsAccount class implementation
    Implemented the SavingsAccount class with constructor, getType, and saveData methods.

---
+++ cpp project/SavingsAccount.cpp
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * File        : SavingsAccount.cpp
+ * Author      : Your Name
+ * Date        : 28/07/2026
+ * Description : Implementation of the SavingsAccount class member functions.
+ *******************************************************************************/
+
+#include "SavingsAccount.h"
+#include <sstream>
+
+ /*******************************************************************************
+  * Function    : SavingsAccount
+  * Description : Constructor for SavingsAccount that passes parameters to the
+  *               Account base class constructor.
+  *
+  * Parameters  :
+  *   id      - Unique identifier for the account.
+  *   owner   - Name of the account holder.
+  *   balance - Initial balance amount.
+  *
+  * Returns     : None.
+  *******************************************************************************/
+SavingsAccount::SavingsAccount(string id, string owner, double balance)
+    : Account(id, owner, balance)
+{
+
+}
+
+/*******************************************************************************
+ * Function    : getType
+ * Description : Returns the specific type name of this account.
+ *
+ * Parameters  : None.
+ *
+ * Returns     : string - "Savings"
+ *******************************************************************************/
+string SavingsAccount::getType() const
+{
+    return "Savings";
+}
+
+/*******************************************************************************
+ * Function    : saveData
+ * Description : Serializes savings account data into a comma-separated string format.
+ *
+ * Parameters  : None.
+ *
+ * Returns     : string - Formatted account details (Type, ID, Owner, Balance).
+ *******************************************************************************/
+string SavingsAccount::saveData() const
+{
+    stringstream ss;
+
+    ss << "Savings,"
+        << id << ","
+        << owner << ","
+        << balance;
+
+    return ss.str();
