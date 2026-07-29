749bd17141add31a55323c5d72372755bc039512
samaralsherbeny <samaralsherbeny2@gmail.com>
2026-07-29 16:16:16 +0300

    Update Account.cpp
    Updated Account class to use int for ID and string for name. Changed method names and added save function for serialization.

--- cpp project/Account.cpp
+++ cpp project/Account.cpp
@@ -1,56 +1,55 @@
 /*******************************************************************************
  * File        : Account.cpp
  * Author      : samar salah 
- * Date        : 27/07/2026
+ * Date        : 29/07/2026
  * Description : Implementation of the Account class member functions.
  *******************************************************************************/
 
 #include "Account.h"
+#include <stdexcept>
+#include<string>
 
  /*******************************************************************************
   * Function    : Account
   * Description : Parameterized constructor for initializing account properties.
   *
   * Parameters  :
-  *   id      - Unique identifier for the account.
-  *   owner   - Name of the account owner.
-  *   balance - Initial balance for the account.
+  *   i - Unique identifier for the account.
+  *   n - Account holder's name.
+  *   b - Initial balance for the account.
   *
   * Returns     : None.
   *******************************************************************************/
-
-Account::Account(string id, string owner, double balance)
-{
-    this->id = id;
-    this->owner = owner;
-    this->balance = balance;
+Account::Account(int i, string n, double b) {
+    id = i;
+    name = n;
+    balance = b;
 }
+
 /*******************************************************************************
  * Function    : getId
  * Description : Retrieves the account ID.
  *
  * Parameters  : None.
  *
- * Returns     : string - The account ID.
+ * Returns     : int - The account ID.
  *******************************************************************************/
-
-string Account::getId() const
-{
+int Account::getId() {
     return id;
 }
+
 /*******************************************************************************
- * Function    : getOwner
- * Description : Retrieves the owner's name.
+ * Function    : getName
+ * Description : Retrieves the account holder's name.
  *
  * Parameters  : None.
  *
- * Returns     : string - The account owner's name.
+ * Returns     : string - The account holder's name.
  *******************************************************************************/
-
-string Account::getOwner() const
-{
-    return owner;
+string Account::getName() {
+    return name;
 }
+
 /*******************************************************************************
  * Function    : getBalance
  * Description : Retrieves the current account balance.
@@ -59,64 +58,36 @@ string Account::getOwner() const
  *
  * Returns     : double - The current balance.
  *******************************************************************************/
-
-double Account::getBalance() const
-{
+double Account::getBalance() {
     return balance;
 }
+
 /*******************************************************************************
  * Function    : deposit
  * Description : Deposits a specified amount into the account. Throws an
- *               exception if the deposit amount is zero or negative.
+ *               invalid_argument exception if the deposit amount is zero or negative.
  *
  * Parameters  :
  *   amount - The amount to be deposited.
  *
  * Returns     : void
  *******************************************************************************/
-
-void Account::deposit(double amount)
-{
+void Account::deposit(double amount) {
     if (amount <= 0)
-        throw invalid_argument("Amount must be greater than zero..");
+        throw invalid_argument("Invalid deposit");
 
     balance += amount;
 }
+
 /*******************************************************************************
- * Function    : withdraw
- * Description : Withdraws a specified amount from the account if valid and
- *               sufficient funds exist. Throws an exception for invalid amounts.
- *
- * Parameters  :
- *   amount - The amount to withdraw.
- *
- * Returns     : bool - True if withdrawal was successful, false if insufficient balance.
- *******************************************************************************/
-
-bool Account::withdraw(double amount)
-{
-    if (amount <= 0)
-        throw invalid_argument("Amount must be greater than zero.");
-
-    if (amount > balance)
-        return false;
-
-    balance -= amount;
-    return true;
-}
-/*******************************************************************************
- * Function    : display
- * Description : Prints account details (ID, Owner, Type, Balance) to standard output.
+ * Function    : save
+ * Description : Serializes account data (ID, name, balance) into a
+ *               comma-separated string format.
  *
  * Parameters  : None.
  *
- * Returns     : void
+ * Returns     : string - Formatted account details.
  *******************************************************************************/
-
-void Account::display() const
-{
-    cout << "ID: " << id << endl;
-    cout << "Owner: " << owner << endl;
-    cout << "Type: " << getType() << endl;
-    cout << "Balance: $" << balance << endl;
+string Account::save() {
+    return to_string(id) + "," + name + "," + to_string(balance);
 }
